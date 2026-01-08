e34b0309ebb06fa0f025ccd493bd9c498ce74887
Jason Zeng <jason.zeng@intel.com>
2026-01-07 20:14:29 -0800

    Revert "fs/resctrl: Fix resctrl mount options display in mount list"
    This reverts commit 11f9972b32685731940ea13000e9743d7faeecd9.

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -3890,12 +3890,10 @@ out:
 
 static int rdtgroup_show_options(struct seq_file *seq, struct kernfs_root *kf)
 {
-	bool l2_visible = !resctrl_arch_get_resource(RDT_RESOURCE_L2)->invisible;
-
 	if (resctrl_arch_get_cdp_enabled(RDT_RESOURCE_L3))
 		seq_puts(seq, ",cdp");
 
-	if (l2_visible && resctrl_arch_get_cdp_enabled(RDT_RESOURCE_L2))
+	if (resctrl_arch_get_cdp_enabled(RDT_RESOURCE_L2))
 		seq_puts(seq, ",cdpl2");
 
 	if (is_mba_sc(resctrl_arch_get_resource(RDT_RESOURCE_MBA)))
@@ -3903,9 +3901,6 @@ static int rdtgroup_show_options(struct seq_file *seq, struct kernfs_root *kf)
 
 	if (resctrl_debug)
 		seq_puts(seq, ",debug");
-
-	if (IS_ENABLED(CONFIG_ARM64_MPAM) && l2_visible)
-		seq_puts(seq, ",l2");
 
 	return 0;
 }
