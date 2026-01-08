0efd259344895150e978a3ec732a27034e440f0e
Jason Zeng <jason.zeng@intel.com>
2026-01-07 20:10:55 -0800

    Revert "fs/resctrl: Fix the crash caused by mounting resctrl but not support RDT"
    This reverts commit bd359a3fe4390bd062e36c271fda4f806f3f2eca.

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -148,7 +148,7 @@ int closids_supported(void)
 	return closid_free_map_len;
 }
 
-static int closid_init(void)
+static void closid_init(void)
 {
 	struct resctrl_schema *s;
 	u32 rdt_min_closid = ~0;
@@ -157,20 +157,12 @@ static int closid_init(void)
 	list_for_each_entry(s, &resctrl_schema_all, list)
 		rdt_min_closid = min(rdt_min_closid, s->num_closid);
 
-	if (rdt_min_closid == ~0)
-		return -EOPNOTSUPP;
-
 	closid_free_map = bitmap_alloc(rdt_min_closid, GFP_KERNEL);
-	if (!closid_free_map)
-		return -ENOMEM;
-
 	bitmap_fill(closid_free_map, rdt_min_closid);
 
 	/* RESCTRL_RESERVED_CLOSID is always reserved for the default group */
 	__clear_bit(RESCTRL_RESERVED_CLOSID, closid_free_map);
 	closid_free_map_len = rdt_min_closid;
-
-	return 0;
 }
 
 static void closid_exit(void)
@@ -2693,9 +2685,7 @@ static int rdt_get_tree(struct fs_context *fc)
 	if (ret)
 		goto out_schemata_free;
 
-	ret = closid_init();
-	if (ret)
-		goto out_schemata_free;
+	closid_init();
 
 	if (resctrl_arch_mon_capable())
 		flags |= RFTYPE_MON;
