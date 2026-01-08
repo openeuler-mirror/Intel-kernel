0de0b8853eab52b0c4ece18930a5240d7b39aa4f
Jason Zeng <jason.zeng@intel.com>
2026-01-07 21:13:23 -0800

    Revert "fs/resctrl: Remove the limit on the number of CLOSID"
    This reverts commit 1e7284b7981ce2b360ec114b5505288664a59a68.

--- drivers/platform/mpam/mpam_resctrl.c
+++ drivers/platform/mpam/mpam_resctrl.c
@@ -132,7 +132,7 @@ static bool mpam_resctrl_hide_cdp(enum resctrl_res_level rid)
  */
 u32 resctrl_arch_get_num_closid(struct rdt_resource *ignored)
 {
-	return mpam_partid_max + 1;
+	return min((u32)mpam_partid_max + 1, (u32)RESCTRL_MAX_CLOSID);
 }
 
 u32 resctrl_arch_system_num_rmid_idx(void)
