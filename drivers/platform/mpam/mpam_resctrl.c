00835401e7aae677b01b35523736d12c1ff6d6ea
Jason Zeng <jason.zeng@intel.com>
2026-05-08 01:41:22 -0700

    Revert "arm64/mpam: Update QoS partition default value"
    Intel inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/8967
    
    ---------------------------
    
    This reverts commit ec8cf750710c847a0b10e9cf6304760e30737e75.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- drivers/platform/mpam/mpam_resctrl.c
+++ drivers/platform/mpam/mpam_resctrl.c
@@ -966,7 +966,6 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		r->schema_fmt = RESCTRL_SCHEMA_RANGE;
 		r->fflags = RFTYPE_RES_MB;
 		r->default_ctrl = MAX_MBA_BW;
-		r->membw.max_bw = MAX_MBA_BW;
 		r->data_width = 3;
 
 		r->membw.delay_linear = true;
@@ -995,7 +994,6 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		r->schema_fmt = RESCTRL_SCHEMA_RANGE;
 		r->fflags = RFTYPE_RES_CACHE;
 		r->default_ctrl = MAX_MBA_BW;
-		r->membw.max_bw = MAX_MBA_BW;
 		r->data_width = 3;
 		r->cache_level = class->level;
 
@@ -1011,8 +1009,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		r->format_str = "%d=%0*u";
 		r->schema_fmt = RESCTRL_SCHEMA_RANGE;
 		r->fflags = RFTYPE_RES_CACHE;
-		r->default_ctrl = 0;
-		r->membw.max_bw = MAX_MBA_BW;
+		r->default_ctrl = MAX_MBA_BW;
 		r->data_width = 3;
 		r->cache_level = class->level;
 
@@ -1027,8 +1024,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		r->format_str = "%d=%0*u";
 		r->schema_fmt = RESCTRL_SCHEMA_RANGE;
 		r->fflags = RFTYPE_RES_MB;
-		r->default_ctrl = 0;
-		r->membw.max_bw = MAX_MBA_BW;
+		r->default_ctrl = MAX_MBA_BW;
 		r->data_width = 3;
 
 		r->membw.delay_linear = true;
@@ -1048,8 +1044,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		r->format_str = "%d=%0*u";
 		r->schema_fmt = RESCTRL_SCHEMA_RANGE;
 		r->fflags = RFTYPE_RES_CACHE;
-		r->default_ctrl = 0;
-		r->membw.max_bw = GENMASK(cprops->intpri_wd - 1, 0);
+		r->default_ctrl = GENMASK(cprops->intpri_wd - 1, 0);
 		r->data_width = 3;
 		r->cache_level = class->level;
 
@@ -1064,8 +1059,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		r->format_str = "%d=%0*u";
 		r->schema_fmt = RESCTRL_SCHEMA_RANGE;
 		r->fflags = RFTYPE_RES_MB;
-		r->default_ctrl = 3;
-		r->membw.max_bw = GENMASK(cprops->intpri_wd - 1, 0);
+		r->default_ctrl = GENMASK(cprops->intpri_wd - 1, 0);
 		r->data_width = 3;
 
 		r->membw.bw_gran = 1;
@@ -1079,7 +1073,6 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		r->schema_fmt = RESCTRL_SCHEMA_RANGE;
 		r->fflags = RFTYPE_RES_MB;
 		r->default_ctrl = 1;
-		r->membw.max_bw = 1;
 		r->data_width = 1;
 
 		r->membw.bw_gran = 1;
