353ae04768dca1eac17db1aecd95764a4f100bdf
Jason Zeng <jason.zeng@intel.com>
2026-05-08 01:43:06 -0700

    Revert "fs/resctrl: Add support for assigning iommu_groups to resctrl groups"
    Intel inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/8967
    
    ---------------------------
    
    This reverts commit 7e3f95a1a8616ccda06e3263c7a6cacb162e07a5.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -422,32 +422,4 @@ void resctrl_exit(void);
 int resctrl_arch_mon_resource_init(void);
 void mbm_config_rftype_init(const char *config);
 
-/* When supported, the architecture must implement these */
-#ifdef CONFIG_RESCTRL_IOMMU
-int resctrl_arch_set_iommu_closid_rmid(struct iommu_group *group, u32 closid,
-				       u32 rmid);
-bool resctrl_arch_match_iommu_closid(struct iommu_group *group, u32 closid);
-bool resctrl_arch_match_iommu_closid_rmid(struct iommu_group *group, u32 closid,
-					  u32 rmid);
-#else
-static inline int
-resctrl_arch_set_iommu_closid_rmid(struct iommu_group *group,
-				   u32 closid, u32 rmid)
-{
-	return -EOPNOTSUPP;
-}
-
-static inline bool
-resctrl_arch_match_iommu_closid(struct iommu_group *group, u32 closid)
-{
-	return false;
-}
-
-static inline bool
-resctrl_arch_match_iommu_closid_rmid(struct iommu_group *group,
-				     u32 closid, u32 rmid)
-{
-	return false;
-}
-#endif /* CONFIG_RESCTRL_IOMMU */
 #endif /* _RESCTRL_H */
