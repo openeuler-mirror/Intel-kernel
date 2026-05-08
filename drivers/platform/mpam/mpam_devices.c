4e186bf8d69aa43bd2de00b8620090ab21255762
Jason Zeng <jason.zeng@intel.com>
2026-05-08 01:43:21 -0700

    Revert "arm64/mpam: Fix out-of-bound access of cfg array"
    Intel inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/8967
    
    ---------------------------
    
    This reverts commit dbb480977ec1f4088f83298fcac7b3623a796b39.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- drivers/platform/mpam/mpam_devices.c
+++ drivers/platform/mpam/mpam_devices.c
@@ -1247,7 +1247,7 @@ struct reprogram_ris {
 /* Call with MSC lock held */
 static int mpam_reprogram_ris(void *_arg)
 {
-	u16 partid, num_partid;
+	u16 partid, partid_max;
 	struct reprogram_ris *arg = _arg;
 	struct mpam_msc_ris *ris = arg->ris;
 	struct mpam_config *cfg = arg->cfg;
@@ -1256,9 +1256,9 @@ static int mpam_reprogram_ris(void *_arg)
 		return 0;
 
 	spin_lock(&partid_max_lock);
-	num_partid = resctrl_arch_get_num_closid(NULL);
+	partid_max = mpam_partid_max;
 	spin_unlock(&partid_max_lock);
-	for (partid = 0; partid < num_partid; partid++)
+	for (partid = 0; partid < partid_max; partid++)
 		mpam_reprogram_ris_partid(ris, partid, cfg);
 
 	return 0;
@@ -1414,7 +1414,7 @@ static void mpam_reprogram_msc(struct mpam_msc *msc)
 		}
 
 		reset = true;
-		for (partid = 0; partid < resctrl_arch_get_num_closid(NULL); partid++) {
+		for (partid = 0; partid < mpam_partid_max; partid++) {
 			cfg = &ris->comp->cfg[partid];
 			if (cfg->features)
 				reset = false;
@@ -2117,8 +2117,7 @@ static int __allocate_component_cfg(struct mpam_component *comp)
 	if (comp->cfg)
 		return 0;
 
-	comp->cfg = kcalloc(resctrl_arch_get_num_closid(NULL),
-			    sizeof(*comp->cfg), GFP_KERNEL);
+	comp->cfg = kcalloc(mpam_partid_max, sizeof(*comp->cfg), GFP_KERNEL);
 	if (!comp->cfg)
 		return -ENOMEM;
 
@@ -2230,7 +2229,7 @@ void mpam_reset_class(struct mpam_class *class)
 
 	idx = srcu_read_lock(&mpam_srcu);
 	list_for_each_entry_rcu(comp, &class->components, class_list) {
-		memset(comp->cfg, 0, resctrl_arch_get_num_closid(NULL) * sizeof(*comp->cfg));
+		memset(comp->cfg, 0, (mpam_partid_max * sizeof(*comp->cfg)));
 
 		list_for_each_entry_rcu(ris, &comp->ris, comp_list) {
 			mutex_lock(&ris->msc->lock);
