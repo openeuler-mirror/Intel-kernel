6848d49b451ef80ac78c6eeeffd0d866df5ef8e2
Jason Zeng <jason.zeng@intel.com>
2026-05-08 01:59:03 -0700

    Revert "arm_mpam: resctrl: Implement resctrl_arch_reset_resources()"
    Intel inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/8967
    
    ---------------------------
    
    This reverts commit 70f9e820a56a45163efbd922d3c86a52dec1be3b.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- include/linux/arm_mpam.h
+++ include/linux/arm_mpam.h
@@ -50,7 +50,4 @@ static inline bool resctrl_arch_is_mbm_total_enabled(void)
 	return false;
 }
 
-/* reset cached configurations, then all devices */
-void resctrl_arch_reset_resources(void);
-
 #endif /* __LINUX_ARM_MPAM_H */
