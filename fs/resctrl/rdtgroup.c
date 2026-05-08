0626917659805514555566284c1e33d78c48a642
Jason Zeng <jason.zeng@intel.com>
2026-05-08 01:41:52 -0700

    Revert "fs/resctrl: Add l2 mount option to enable L2 msc"
    Intel inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/8967
    
    ---------------------------
    
    This reverts commit 8bf66c9a68ffa5be9c033f75cc8f31a4ab7d7219.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -2517,7 +2517,6 @@ static void rdt_disable_ctx(void)
 
 static int rdt_enable_ctx(struct rdt_fs_context *ctx)
 {
-	struct rdt_resource *r;
 	int ret = 0;
 
 	if (ctx->enable_cdpl2) {
@@ -2540,13 +2539,6 @@ static int rdt_enable_ctx(struct rdt_fs_context *ctx)
 
 	if (ctx->enable_debug)
 		resctrl_debug = true;
-
-	r = resctrl_arch_get_resource(RDT_RESOURCE_L2);
-	/* Only arm64 arch hides L2 resource by default */
-	if (IS_ENABLED(CONFIG_ARM64_MPAM) && !ctx->enable_l2)
-		r->invisible = true;
-	else
-		r->invisible = false;
 
 	return 0;
 
@@ -2770,7 +2762,6 @@ enum rdt_param {
 	Opt_cdpl2,
 	Opt_mba_mbps,
 	Opt_debug,
-	Opt_l2,
 	nr__rdt_params
 };
 
@@ -2779,7 +2770,6 @@ static const struct fs_parameter_spec rdt_fs_parameters[] = {
 	fsparam_flag("cdpl2",		Opt_cdpl2),
 	fsparam_flag("mba_MBps",	Opt_mba_mbps),
 	fsparam_flag("debug",		Opt_debug),
-	fsparam_flag("l2",		Opt_l2),
 	{}
 };
 
@@ -2807,9 +2797,6 @@ static int rdt_parse_param(struct fs_context *fc, struct fs_parameter *param)
 		return 0;
 	case Opt_debug:
 		ctx->enable_debug = true;
-		return 0;
-	case Opt_l2:
-		ctx->enable_l2 = true;
 		return 0;
 	}
 
@@ -3161,9 +3148,6 @@ static int mkdir_mondata_all(struct kernfs_node *parent_kn,
 	for (i = 0; i < RDT_NUM_RESOURCES; i++) {
 		r = resctrl_arch_get_resource(i);
 		if (!r->mon_capable)
-			continue;
-
-		if (r->invisible)
 			continue;
 
 		ret = mkdir_mondata_subdir_alldom(kn, r, prgrp);
