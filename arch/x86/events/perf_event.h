0f65871d636c0d0ca2cefcea33d40009659875fa
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-07-17 16:03:41 +0800

    perf/x86: Optimize ACR handling in match_prev_assignment()
    mainline inclusion
    from mainline-v7.3-rc1
    commit 917d558b151cad5b05991e5eaee22efab33525ca
    category: bugfix
    bugzilla: https://atomgit.com/openeuler/kernel/issues/10011
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=917d558b151cad5b05991e5eaee22efab33525ca
    
    ---------------------------
    
    match_prev_assignment() currently forces a mismatch for ACR events, so
    ACR counter indices are reprogrammed on every scheduling pass. That
    causes avoidable overhead because disable and enable paths must touch
    multiple MSRs.
    
    The previous ACR assignment is already cached in acr_cfg_b[]. Use that
    state to compare the newly computed ACR counter indices in hwc->config1
    against the cached value in acr_cfg_b[hwc->idx]. If they match, skip
    unnecessary disable and enable work.
    
    Also tighten is_acr_self_reload_event() so it first verifies the event
    is an ACR event before testing for the self-reload case.
    
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Thomas Falcon <thomas.falcon@intel.com>
    Reviewed-by: Zide Chen <zide.chen@intel.com>
    Link: https://patch.msgid.link/20260717080342.1879573-8-dapeng1.mi@linux.intel.com
    [jz: use event->hw_ext to access config1]
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -141,7 +141,7 @@ static inline bool is_acr_self_reload_event(struct perf_event *event)
 	struct hw_perf_event *hwc = &event->hw;
 	struct hw_perf_event_ext *hwc_ext = event->hw_ext;
 
-	if (hwc->idx < 0)
+	if (hwc->idx < 0 || !is_acr_event_group(event))
 		return false;
 
 	return test_bit(hwc->idx, (unsigned long *)&hwc_ext->config1);
